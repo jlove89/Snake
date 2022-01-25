1de21e8088bf039875455f8e0e7279bce9222d34
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-01-24 21:09:51 -0500

    test snake speed

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 let lastRenderTime = 0
+const SNAKE_SPEED = 2
 
 function main(currentTime) {
-    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
     window.requestAnimationFrame(main)
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+    if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
+    
+    
+    console.log('test')
     lastRenderTime = currentTime
-    console.log(secondsSinceLastRender)
 }
 
 window.requestAnimationFrame(main)
