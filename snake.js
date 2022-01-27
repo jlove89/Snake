c3e40a16c581593f46e6452fd1a08788b875fbd1
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-01-26 20:49:51 -0500

    it moves!

--- snake.js
+++ snake.js
@@ -1,18 +1,19 @@
+import { getInputDirection } from "./input.js"
+
 export const SNAKE_SPEED = 1
-const snakeBody =  [
-    { x: 10, y: 11 },
-    { x: 11, y: 11 },
-    { x: 12, y: 11 },
-    { x: 13, y: 11 },
-    { x: 14, y: 11 }
+const snakeBody =  [{ x: 11, y: 11 },
+    //{ x: 12, y: 11 },
+    //{ x: 13, y: 11 },
+    //{ x: 14, y: 11 }
 ]
 
 export function update() {
-for (let i =  snakeBody.length - 2; i >= 0; i--) {
+    const inputDirection = getInputDirection()
+    for (let i =  snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] }
 }
-    // snakeBody[0].x += 0
-    // snakeBody[0].y += 1
+    snakeBody[0].x += inputDirection.x
+    snakeBody[0].y += inputDirection.y
     
     //console.log('update snake')
 }
