fd30d678e6dfa567af2a9fcf43372a9a2df0fdf9
Jeremy Love <pristinesolutionsnc@gmail.com>
2022-01-26 19:08:15 -0500

    snake appears

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-import { SNAKE_SPEED } from './snake.js'
+import { update as updateSnake, draw as drawSnake, SNAKE_SPEED } from './snake.js'
+
 let lastRenderTime = 0
+const gameBoard = document.getElementById('game-board')
 
 function main(currentTime) {
     window.requestAnimationFrame(main)
@@ -7,7 +9,7 @@ function main(currentTime) {
     if (secondsSinceLastRender < 1 / SNAKE_SPEED) return
     
     
-    console.log('test')
+    //console.log('test')
     lastRenderTime = currentTime
 
     update()
@@ -17,9 +19,9 @@ function main(currentTime) {
 window.requestAnimationFrame(main)
 
 function update() {
-
+    updateSnake()
 }
 
 function draw() {
-
+    drawSnake(gameBoard)
 }
--- snake.js
+++ snake.js
@@ -1,9 +1,21 @@
 export const SNAKE_SPEED = 1
+const snakeBody =  [
+    { x: 10, y: 11 },
+    { x: 11, y: 11 },
+    { x: 12, y: 11 }
 
-function update() {
+]
 
+export function update() {
+    console.log('update snake')
 }
 
-function draw() {
-
+export function draw(gameBoard) {
+    snakeBody.forEach(segment => {
+        const snakeElement = document.createElement('div')
+        snakeElement.style.gridRowStart = segment.x
+        snakeElement.style.gridColumnStart = segment.y
+        snakeElement.classList.add('snake')
+        gameBoard.appendChild(snakeElement)
+    })
 }
